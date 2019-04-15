05f9bf7d4d8d9fe228806b55bd1e2fd50392c924
ghar <ghar@shans-MBP.domain>
2019-04-14 23:41:19 -0700

    adding testing.py

---
+++ testing.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+pd.set_option('max_columns', 50)
+#%matplotlib inline
+
+# create a Series with an arbitrary list
+s = pd.Series([7, 'Heisenberg', 3.14, -1789710578, 'Happy Eating!'])
+print(s)
